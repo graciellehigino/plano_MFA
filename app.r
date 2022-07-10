3a9473f0632c064cbdca8bec716206b77d59b323
Gracielle Higino <graciellehigino@hotmail.com>
2022-07-09 18:24:58 -0700

    clean app script

--- app.r
+++ app.r
@@ -1,23 +1,9 @@
 library(shiny)
 library(timevis)
 source("data.R")
+source("utils.R")
+
 ui <- source("ui.R")
 server <- source("server.R")
 
-ui <- fluidPage(
-  timevisOutput("mytime"),
-  actionButton("btn", "Add item and center")
-)
-
-server <- function(input, output, session) {
-  output$mytime <- renderTimevis(timevis(data))
-  observeEvent(input$btn, {
-    addItem("mytime", list(id = "admission", content = "one", start = "2022-08-01"))
-    centerItem("mytime", "admission")
-  })
-}
-
 shinyApp(ui = ui$value, server = server$value)
-
-shinyApp(ui = ui, server = server)
-
