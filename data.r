1a2abe7eb4501bf0f9e51bd21ab9fa3d61488eac
Gracielle Higino <graciellehigino@hotmail.com>
2022-07-13 15:07:06 -0700

    update dataset

--- data.r
+++ data.r
@@ -1,20 +1,12 @@
 library(timevis)
 
 data <- data.frame(
-  id      = 1:3,
-  content = c("Write & send pitch to faculty",
-              "Trip MTL",
-              "Schedule English test"),
-  start   = c("2022-08-10", "2022-08-13", "2022-08-29"),
-  end     = c("2022-08-12", "2022-08-27", NA),
-  group = c("admission", "documentary", "admission"),
-  type = c(rep("range", 2), "point"),
-  editable = TRUE
+ read.csv("data.csv", header = TRUE)
 )
 
-timevis(data)
+# timevis(data)
 
-timevisDataGroups <- data.frame(
-  id = c("admission", "documentary"),
-  content = c("Admissions", "Documentary")
-)
+# # timevisDataGroups <- data.frame( 
+#   id = levels(as.factor(data$group)),
+#   content = levels(as.factor(data$group))
+# )
